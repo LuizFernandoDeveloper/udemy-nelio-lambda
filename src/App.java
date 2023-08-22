27d4c4843de578c4a5d67a4bf79294d2eed44c0f
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-08-22 15:32:10 -0300

    initial

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import model.Product;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        Collections.sort(list);
+
+        for (Product product : list) {
+            System.out.println(product);
+        }
     }
 }
