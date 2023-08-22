be1dc4e7a42376cf8dc292615e2f97f9d200728a
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-08-22 15:43:40 -0300

    classe anonima

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import model.Product;
@@ -11,7 +12,16 @@ public class App {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        list.sort(new MyComparator());
+        Comparator <Product> comp = new Comparator<Product>() {
+
+            @Override
+            public int compare(Product o1, Product o2) {
+                return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
+            }
+            
+        };
+
+        list.sort(comp);
 
         for (Product product : list) {
             System.out.println(product);
